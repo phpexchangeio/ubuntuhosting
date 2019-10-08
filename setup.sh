78662bcdda03574b5c61a140a5673d605523c3d5
jhagan91 <55489000+jhagan91@users.noreply.github.com>
2019-10-08 23:42:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+!/bin/sh
 
 echo "Welcome to the Ubuntu Hosting Environment Setup Script"
 echo
 echo "Checking if Ubuntu is installed on the server..."
-lsb_release -a
+if [[ `lsb_release -rs` == "19.04" ]]
+echo "Great you are running Ubuntu 19.04..."
+  else
+  exit 1
+fi
+
+echo "Test Text"
+
+
