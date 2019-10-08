e617870d3b5e5f19457722b57be86b7585a4fd64
jhagan91 <55489000+jhagan91@users.noreply.github.com>
2019-10-08 23:46:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo
 echo "Checking if Ubuntu is installed on the server..."
 if [[ `lsb_release -rs` == "19.04" ]];
 then
-echo "Great you are running Ubuntu 19.04..."
+echo "Great you are running Ubuntu 19.04...";
 else
   exit 1
 fi
